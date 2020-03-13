cf477164bc81f26c1c9d506f5acfe6cd8b955da3
Jeff Reeve <54605252+JefReeve@users.noreply.github.com>
2020-03-12 20:59:26 -0500

    Update test.cpp

--- Assignments/H05/test.cpp
+++ Assignments/H05/test.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include "playingCard.hpp"
-#include "termio.h"
+#include "TrIO.hpp"
 
-//using namespace std;
-using namespace Term;
+using namespace std;
+//using namespace Term;
 
 int main()
 {
+  
+  Term::IO io;
+
     Card c(0);
     Card d(0);
 
-    d.setBackgroundColor("Magenta");
-    d.setForegroundColor("Yellow");
-    d.setSuitColor("Blue");
-    d.setRankColor("Cyan");
+    std::string card1 = "";
+    card1 += "&28┌──┐\n";
+    card1 += "&28│D9│\n";
+    card1 += "&28└──┘\n";
+
+
+    d.setBackgroundColor("Black");
+    d.setForegroundColor("Green");
+    d.setSuitColor("Yellow");
+    d.setRankColor("Magenta");
     
-    cout << "\n" << c << "\n\n";
-    cout << "\n" << d << "\n\n";
+    io << "\n" << c << "\n\n";
+    io << "\n" << d << "\n\n";
+
+    io << card1;
 }
